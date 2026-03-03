61cca90ef5ba59fd19b5df15fcf39011b6146bfd
Harsh <hk755063@gmail.com>
2026-03-04 03:26:08 +0530

    api connected to db

--- src/controllers/identify.controller.ts
+++ src/controllers/identify.controller.ts
@@ -1,5 +1,31 @@
 import { Request, Response } from "express";
+import prisma from "../lib/prisma";
 
-export const identifyPerson = (req: Request, res: Response) => {
-    res.json({message: "Person identified"})
-} 
+export const identifyPerson = async (req: Request, res: Response) => {
+  try {
+    console.log("hello!!");
+    const { phoneNumber, email, linkPrecedence } = req.body;
+
+    if (!phoneNumber && !email) {
+      return res.status(400).json({
+        message: "phoneNumber or email is required",
+      });
+    }
+
+    const contact = await prisma.contact.create({
+      data: {
+        phoneNumber: phoneNumber || null,
+        email: email || null,
+        linkPrecedence: linkPrecedence || "primary",
+      },
+    });
+
+    return res.status(201).json(contact);
+  } catch (error) {
+    console.error("Error creating contact:", error);
+
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
-import express, {Request , Response} from "express";
+import express, { Request, Response } from "express";
 import identifyRoutes from "./routes/identify.route";
+import dotenv from "dotenv";
 
-const app = express()
-const PORT = 9000
+const app = express();
+const PORT = 9000;
 
 app.use(express.json());
+dotenv.config();
 
 app.use("/api/", identifyRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Server is running...");
-})
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running...");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+  console.log(`Server running at http://localhost:${PORT}`);
+});
